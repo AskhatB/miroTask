33931c3f241a72b78926dd175dee85df569da9f9
Askhat <baltabaev.a2509@gmail.com>
2019-07-23 15:02:39 +0600

    feat: execute index.html like app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(4500, () => {
+  console.log('App listen on port 4500');
+});
